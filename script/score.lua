1bc2647734c9f955ff648ab084aeb34e1c4a8908
最萌小汐 <sumneko@hotmail.com>
2020-06-24 21:55:03 +0800

    连接排行榜

--- script/score.lua
+++ script/score.lua
@@ -44,10 +44,25 @@ end
 function m.hello(data)
     local ip = ngx.var.remote_addr
     return doRedis(function (red)
-        red:hincrby('hello', ip, 1)
+        red:zincrby('hello', 1, ip)
+        local count = red:zscore('hello', ip)
+        local max = 10
+        local list = red:zrevrange('hello', -max, -1, 'WITHSCORES')
+        local lines = {}
+        lines[#lines+1] = '======= 排行榜 ======='
+        for i = 1, max do
+            local name = list[i * 2 - 1]
+            local score = list[i * 2]
+            if not name then
+                break
+            end
+            lines[#lines+1] = ('%d. %s: 连接了 %s 次'):format(i, name, score)
+        end
+        lines[#lines+1] = '==================='
+
         return {
             type = 'print',
-            value = ('你好，来自 %s 的 %s ，你已成功连接服务器。这是你的第 %d 次连接。'):format(ip, data.value, red:hget('hello', ip))
+            value = ('你好，来自 %s 的 %s ，你已成功连接服务器。这是你的第 %d 次连接。\n%s'):format(ip, data.value, count, table.concat(lines, '\n'))
         }
     end)
 end
