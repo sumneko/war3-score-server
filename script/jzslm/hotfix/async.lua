88d57f8cae506a7c9353f96296ba4d8987b268b8
最萌小汐 <sumneko@hotmail.com>
2020-08-06 18:50:08 +0800

    热更新脚本

--- script/jzslm/hotfix/async.lua
+++ script/jzslm/hotfix/async.lua
@@ -1,51 +1,55 @@
-local jass   = require 'jass.common'
-local japi   = require 'jass.japi'
-local record = require 'ac.record'
-
-local function reportCheat()
-    print('作弊')
-    record.request('common', 'cheat', {
-        ['player'] = ac.localPlayer():userName(),
-        ['value']  = 10,
-    })
+if _0x07E2_ then
+return
 end
 
-local function getAttackSpeed(u)
-    local v = u:get '攻击速度'
-    if v >= 0 then
-        return 1 + v / 100
-    else
-        --当攻击速度小于0的时候,每点相当于攻击间隔增加1%
-        return 1 + v / (100 - v)
-    end
+local _0x00AA_=_0x009C_'_0f0009_'
+local _0x01C1_=_0x009C_'_0f001B_'
+local _0x0CCD_=_0x009C_'_0f020E_'
+
+local function _0x0D0D_()
+_0x009F_("\xE4\xBD\x9C\xE5\xBC\x8A")
+_0x0CCD_._0x092C_("\x63\x6F\x6D\x6D\x6F\x6E","\x63\x68\x65\x61\x74",{
+["\x70\x6C\x61\x79\x65\x72"]=_0x0005_._0x028A_():_0x0782_(),
+["\x76\x61\x6C\x75\x65"]=0xA,
+})
 end
 
-ac.loop(10, function ()
-    local u = ac.localSelectedUnit()
-    if not u then
-        return
-    end
-    if u:getOwner() ~= ac.localPlayer() then
-        return
-    end
-    -- 检查无敌
-    if not u:hasRestriction '无敌' and jass.GetUnitAbilityLevel(u._handle, ac.id['Avul']) > 0 then
-        reportCheat()
-        return
-    end
-    -- 检查攻击间隔
-    if math.abs(u:get '攻击间隔' - japi.GetUnitState(u._handle, 0x25)) > 0.01 then
-        reportCheat()
-        return
-    end
-    -- 检查攻击速度
-    if math.abs(getAttackSpeed(u) - japi.GetUnitState(u._handle, 0x51)) > 0.01 then
-        reportCheat()
-        return
-    end
-    -- 检查攻击范围
-    if math.abs(u:get '攻击范围' - japi.GetUnitState(u._handle, 0x16)) > 0.01 then
-        reportCheat()
-        return
-    end
+local function _0x0D0E_(_0x0038_)
+local _0x017E_=_0x0038_:_0x0036_"\xE6\x94\xBB\xE5\x87\xBB\xE9\x80\x9F\xE5\xBA\xA6"
+if _0x017E_>=0x0 then
+return 0x1+_0x017E_/0x64
+else
+
+return 0x1+_0x017E_/(0x64-_0x017E_)
+end
+end
+
+_0x0005_._0x0063_(0xA,function()
+local _0x0038_=_0x0005_._0x08C5_()
+if not _0x0038_ then
+return
+end
+if _0x0038_:_0x0008_()~=_0x0005_._0x028A_()then
+return
+end
+
+if not _0x0038_:_0x0244_"\xE6\x97\xA0\xE6\x95\x8C"and _0x00AA_._0x058C_(_0x0038_._0x01CE_,_0x0005_._0x009E_["\x41\x76\x75\x6C"])>0x0 then
+_0x0D0D_()
+return
+end
+
+if _0x0086_._0x02BF_(_0x0038_:_0x0036_"\xE6\x94\xBB\xE5\x87\xBB\xE9\x97\xB4\xE9\x9A\x94"-_0x01C1_._0x0D0F_(_0x0038_._0x01CE_,0x25))>0x1.47ae147ae147bp-7 then
+_0x0D0D_()
+return
+end
+
+if _0x0086_._0x02BF_(_0x0D0E_(_0x0038_)-_0x01C1_._0x0D0F_(_0x0038_._0x01CE_,0x51))>0x1.47ae147ae147bp-7 then
+_0x0D0D_()
+return
+end
+
+if _0x0086_._0x02BF_(_0x0038_:_0x0036_"\xE6\x94\xBB\xE5\x87\xBB\xE8\x8C\x83\xE5\x9B\xB4"-_0x01C1_._0x0D0F_(_0x0038_._0x01CE_,0x16))>0x1.47ae147ae147bp-7 then
+_0x0D0D_()
+return
+end
 end)
